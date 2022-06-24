f74be0d21be1b6d85d2143c39410eac3c15e89aa
Human Human <prasetiautama2@gmail.com>
2022-06-24 18:56:43 +0200

    Add some comments to the program

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -4,24 +4,20 @@ from Views.Dialog import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QThread
 import serial
-from Libs.Sender import Sender, UDPSender
+from Libs.Sender import TTLSender, SendReadUDP
 from Libs.Conf import Tobii
 import datetime
+
 
 class MainController:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.view = Ui_MainWindow()
 
-        self.sender = Sender()
+        # ttl sender
 
-
-        # action of start and stop button
-        self.view.startButton.clicked.connect(self.startRecording)
-        self.view.stopButton.clicked.connect(self.stopRecording)
-
-        # action when sender is ready
-        self.sender.run_output.connect(self.onProgress)
+        self.ttl_sender = TTLSender()
+        # set serial for TTL sender
         self.ser = serial.Serial(
             port=Tobii.port_name,  # please make sure the port name is correct
             baudrate=115200,  # maximum baud rate 115200
@@ -29,33 +25,16 @@ class MainController:
             stopbits=serial.STOPBITS_ONE,
             timeout=0
         )
-        self.sender.setSerial(self.ser)
+        self.ttl_sender.setSerial(self.ser)
 
         # vicon
-        self.udp_sender = UDPSender()
-        self.udp_sender.is_started.connect(self.sync)
-        self.udp_sender.start(priority=QThread.HighestPriority)
-
-    def startRecording(self):
-        self.view.startButton.setEnabled(False)
-
-
+        self.udp_send_read = SendReadUDP()
+        self.udp_send_read.is_started.connect(self.sync)
+        self.udp_send_read.start(priority=QThread.HighestPriority)
 
     def sync(self):
-        self.sender.start(priority=QThread.HighestPriority)
-        self.sender.stop()
-
-    def stopRecording(self):
-        self.view.stopButton.setEnabled(False)
-
-
-    def onProgress(self, i):
-        # print(i)
-        if i == 1:
-            self.view.stopButton.setEnabled(True)
-        else:
-            self.view.startButton.setEnabled(True)
-
+        self.ttl_sender.start(priority=QThread.HighestPriority) # send ttl
+        self.ttl_sender.stop() # stop
 
 
 
--- Libs/Sender.py
+++ Libs/Sender.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from Libs.Conf import VICON
 import datetime
 import winsound
+import serial
 
 HIGH = 255
 LOW = 0
@@ -13,7 +14,12 @@ LOW = 0
 
 # HIGH = b'1'
 # LOW = b'Q'
-class Sender(QThread):
+def timeNow():
+    timenow = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
+    return timenow
+
+
+class TTLSender(QThread):
     run_output = pyqtSignal(int)
     time = Event()
 
@@ -21,43 +27,18 @@ class Sender(QThread):
         QThread.__init__(self)
         self._isRunning = False
 
-    def setSerial(self, ser):
+    def setSerial(self, ser: serial.Serial):
         self.ser = ser
 
     def run(self) -> None:
-
         # send the information we want to send
         # to start, we need A rising edge (or positive edge) is the low-to-high transition
 
-
-        # for i in range(3):
-        #     for t in ([HIGH, LOW]):
-        #         self.ser.write(t)
-        #         self.time.wait(100/1000)
-
-        # if not self._isRunning:
-
-        timenow = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
-        print("sending ttl at: "+ timenow)
+        print("sending ttl at: " + timeNow())
         self.ser.write(HIGH)
         self.time.wait(100. / 1000)
         self.ser.write(LOW)
         self.playNotification()
-            # self.time.wait(100 / 1000)
-            # self.ser.write(HIGH)
-            # self.time.wait(3.)
-            # self.ser.write(LOW)
-            # self.time.wait(100 / 1000)
-            # self.ser.write(HIGH)
-            # self._isRunning = True
-            # self.run_output.emit(1)
-        # while True:
-        #     if self._isRunning:
-        #         self.ser.write(HIGH)
-        #         self.time.wait(1)
-        #         self.ser.write(LOW)
-        #     else:
-        #         break
 
     def stop(self):
         self._isRunning = False
@@ -65,138 +46,95 @@ class Sender(QThread):
         self.time.clear()
         self.run_output.emit(0)
 
-
     def playNotification(self):
         frequency = 2500  # Set Frequency To 2500 Hertz
         duration = 1000  # Set Duration To 1000 ms == 1 second
         winsound.Beep(frequency, duration)
 
 
-class UDPSender (QThread):
-
+class SendReadUDP(QThread):
     is_started = pyqtSignal(int)
 
-    def __init__(self):
+    def __init__(self, is_read: bool = True):
         QThread.__init__(self)
+        print("Init socket binding")
+        self.is_read = is_read
+        self.sock = socket.socket(socket.AF_INET,  # Internet
+                                  socket.SOCK_DGRAM)  # UDP
 
-    # def send(self):
-    #     print("Init socket binding: ")
-    #
-    #     sock = socket.socket(socket.AF_INET,  # Internet
-    #                          socket.SOCK_DGRAM)  # UDP
-    #
-    #     print("Start sending UDP Message: ")
-    #     # a = sock.connect((VICON.UDP_IP, VICON.UDP_PORT))
-    #
-    #     packet_ID = 0
-    #     # Let's send data through UDP protocol
-    #
-    #     while True:
-    #         packet_ID += 1
-    #         #### get current time in format HHMMSS
-    #         timenow = datetime.datetime.now().time().strftime("%H%M%S")
-    #         #### simple message, please note the variables. Using the current time allows
-    #         #### having unique ID for the trial. If the recording exist with the current
-    #         #### file name, the recording won't start unless in Nexus the "Permit overwrite
-    #         #### existing files" is ticked.
-    #         test_message = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" \
-    #                        "<CaptureStart>" \
-    #                        "<Name VALUE=\""+VICON.TRIAL_NAME+""+str(timenow)+"\"/>" \
-    #                        "<Notes VALUE=\"\"/>" \
-    #                         "<Description VALUE=\"\"/>" \
-    #                        "<DatabasePath VALUE=\""+VICON.DATABASE_PATH+"\"/>" \
-    #                        "<Delay VALUE = \"250\"/>" \
-    #                        "<PacketID VALUE=\""+str(packet_ID)+"\"/>" \
-    #                         "</CaptureStart>"
-    #         try:
-    #             sock.sendto(test_message.encode(), (VICON.UDP_IP, VICON.UDP_PORT))
-    #              self.is_started.emit(1)
-    #         except socket.error:
-    #             # If no data is received, you get here, but it's not an error
-    #             # Ignore and continue
-    #             print(socket.error)
-    #             pass
+    def send(self):
 
+        print("Start sending UDP Message: ")
+        # Let's send data through UDP protocol
+        packet_ID = 0
+        while True:
+            packet_ID += 1
+            #### get current time in format HHMMSS
+            timenow = datetime.datetime.now().time().strftime("%H%M%S")
+            #### simple message, please note the variables. Using the current time allows
+            #### having unique ID for the trial. If the recording exist with the current
+            #### file name, the recording won't start unless in Nexus the "Permit overwrite
+            #### existing files" is ticked.
+            test_message = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" \
+                           "<CaptureStart>" \
+                           "<Name VALUE=\"" + VICON.TRIAL_NAME + "" + str(timenow) + "\"/>" \
+                                                                                     "<Notes VALUE=\"\"/>" \
+                                                                                     "<Description VALUE=\"\"/>" \
+                                                                                     "<DatabasePath VALUE=\"\"/>" \
+                                                                                     "<Delay VALUE = \"-20\"/>" \
+                                                                                     "<PacketID VALUE=\"" + str(
+                packet_ID) + "\"/>" \
+                             "</CaptureStart>"
 
-    def run(self) -> None:
-        print("Init socket binding: ")
+            try:
+                self.sock.sendto(test_message.encode(), (VICON.UDP_IP, VICON.UDP_PORT))
+                self.is_started.emit(1)
+                break
 
-        sock = socket.socket(socket.AF_INET,  # Internet
-                             socket.SOCK_DGRAM)  # UDP
-        sock.bind(("", VICON.UDP_PORT))
-        sock.setblocking(0)
+            except socket.error:
+
+                pass
+            time.sleep(10)
+
+    def read(self):
+
+        self.sock.bind(("", VICON.UDP_PORT))
+        self.sock.setblocking(0)
         while True:
             try:
-
                 # Attempt to receive up to 1024 bytes of data
-                data, addr = sock.recvfrom(300)
+                data, addr = self.sock.recvfrom(300)
                 # print(data)
                 # Echo the data back to the sender
                 if "CaptureStart" in data.decode("utf-8"):
-                    timenow = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
-                    print("Nexus is started at: " + timenow)
+                    print("Nexus is started at: " + timeNow())
                     self.is_started.emit(1)
                     # break
                 elif "CaptureStop" in data.decode("utf-8"):
-                    timenow = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
-                    print("Nexus is stop at: " + timenow)
+                    print("Nexus is stop at: " + timeNow())
                     self.is_started.emit(1)
 
             except socket.error:
                 # If no data is received, you get here, but it's not an error
                 # Ignore and continue
                 pass
-            time.sleep(1./10000000)
+            time.sleep(1. / 10000000)
 
+    def run(self) -> None:
+        if self.is_read:
+            self.read()
+        else:
+            self.send()
 
-    # def run(self) -> None:
-    #
-    #     print("Init socket binding: ")
-    #
-    #     sock = socket.socket(socket.AF_INET,  # Internet
-    #                          socket.SOCK_DGRAM)  # UDP
-    #
-    #     print("Start sending UDP Message: ")
-    #     sock.connect((VICON.UDP_IP, VICON.UDP_PORT))
-    #
-    #     # Let's send data through UDP protocol
-    #     packet_ID = 0
-    #     while True:
-    #         packet_ID += 1
-    #         #### get current time in format HHMMSS
-    #         timenow = datetime.datetime.now().time().strftime("%H%M%S")
-    #         #### simple message, please note the variables. Using the current time allows
-    #         #### having unique ID for the trial. If the recording exist with the current
-    #         #### file name, the recording won't start unless in Nexus the "Permit overwrite
-    #         #### existing files" is ticked.
-    #         test_message = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" \
-    #                        "<CaptureStart>" \
-    #                        "<Name VALUE=\""+VICON.TRIAL_NAME+""+str(timenow)+"\"/>" \
-    #                        "<Notes VALUE=\"\"/>" \
-    #                         "<Description VALUE=\"\"/>" \
-    #                        "<DatabasePath VALUE=\"\"/>" \
-    #                        "<Delay VALUE = \"-20\"/>" \
-    #                        "<PacketID VALUE=\""+str(packet_ID)+"\"/>" \
-    #                         "</CaptureStart>"
-    #
-    #         try:
-    #             sock.sendall(test_message.encode())
-    #             break
-    #             # self.is_started.emit(1)
-    #         except socket.error:
-    #             # If no data is received, you get here, but it's not an error
-    #             # Ignore and continue
-    #             print(socket.error)
-    #             pass
-    #         time.sleep(10)
-
-
+    def stop(self):
+        self.sock.detach()
 
 
 if __name__ == "__main__":
     import serial
     from Libs.Sender import Sender, UDPSender
     from Libs.Conf import Tobii
+
     sender = Sender()
     ser = serial.Serial(
         port=Tobii.port_name,  # please make sure the port name is correct
